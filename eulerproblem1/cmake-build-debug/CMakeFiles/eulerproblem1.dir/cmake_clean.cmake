e48be21b8c1f898cf4269948bd16bfc173f9866b
Anand <nandopatel2001@yahoo.co.uk>
2017-09-08 23:16:56 +0100

    Add files via upload

---
+++ eulerproblem1/cmake-build-debug/CMakeFiles/eulerproblem1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eulerproblem1.dir/main.cpp.o"
+  "eulerproblem1.pdb"
+  "eulerproblem1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eulerproblem1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
